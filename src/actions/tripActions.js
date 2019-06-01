af45b72707c151c549bd50bb761193c754d620c2
Bruno Vukasovic <vukasovic.bruno@gmail.com>
2019-06-02 00:55:36 +0200

    actions folder removed

--- src/actions/tripActions.js
+++
@@ -1,7 +0,0 @@
-import { GET_BOOKED_TRIPS } from "./types";
-
-export const getBookedTrips = () => {
-  return {
-    type: GET_BOOKED_TRIPS
-  };
-};
--- src/actions/types.js
+++
@@ -1,1 +0,0 @@
-export const GET_BOOKED_TRIPS = "GET_BOOKED_TRIPS";
